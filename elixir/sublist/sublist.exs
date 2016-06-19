765766026abf651e09f4a9a55a434094dfea4943
Eric Biven <eric@biven.us>
2016-06-18 22:32:28 -0500

    sublist using Stream

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,17 +3,18 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, a),      do: :equal
-  def compare([], [nil]), do: :sublist
-  def compare([nil], []), do: :superlist
-
   def compare(a, b) do
-    s_a = "," <> Enum.join(a, ",") <> ","
-    s_b = "," <> Enum.join(b, ",") <> ","
     cond do
-      String.contains?(s_b, s_a) -> :sublist
-      String.contains?(s_a, s_b) -> :superlist
-      true                       -> :unequal
+      a === b        -> :equal
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true           -> :unequal
     end
+  end
+
+  def sublist?([], _), do: true
+  def sublist?(a, b) do
+    Stream.chunk(b, length(a), 1)
+    |> Enum.any?(&(&1 === a))
   end
 end
