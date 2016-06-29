ef8e93d015bc0f0e3a237762379c58f305ff915a
Eric Biven <eric@biven.us>
2016-06-29 08:19:19 -0500

    Updated run-length-encoding
    Removed unused functions, made internal functions private.

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -7,7 +7,6 @@ defmodule RunLengthEncoder do
   "1H1O1R1S1E" => "HORSE"
   """
   @spec encode(String.t) :: String.t
-  def encode(""), do: ""
   def encode(string) do
     string
     |> String.split("", trim: true)
@@ -15,12 +14,11 @@ defmodule RunLengthEncoder do
     |> Enum.reduce("", &encode_reducer/2)
   end
 
-  def encode_reducer(x = [head|_tail], acc) do
+  defp encode_reducer(x = [head|_tail], acc) do
     acc <> "#{Enum.count(x)}#{head}"
   end
 
   @spec decode(String.t) :: String.t
-  def decode(""), do: ""
   def decode(string) do
     string
     |> split
@@ -28,9 +26,9 @@ defmodule RunLengthEncoder do
     |> Enum.reduce("", &decode_reducer/2)
   end
 
-  def split(string), do: Regex.scan(~r/\d+[A-Z]/u, string)
+  defp split(string), do: Regex.scan(~r/\d+[A-Z]/u, string)
 
-  def decode_reducer(item, acc) do
+  defp decode_reducer(item, acc) do
     {times, letter} = Integer.parse(item)
     acc <> String.duplicate(letter, times)
   end
