ec1f3494ee6ca39679b8a0240db87f6ceb47da32
Eric Biven <eric@biven.us>
2016-06-29 07:52:34 -0500

    Updated bob.
    Cleaner code, passes new test.

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,10 +1,17 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      String.ends_with?(input, "?") -> "Sure."
-      String.strip(input) == "" -> "Fine. Be that way!"
-      String.upcase(input) == input && String.downcase(input) != input -> "Whoa, chill out!"
-      true -> "Whatever."
+      question?(input) -> "Sure."
+      nothing?(input)  -> "Fine. Be that way!"
+      yelling?(input)  -> "Whoa, chill out!"
+      true             -> "Whatever."
     end
+  end
+
+  defp question?(input), do: String.ends_with?(input, "?")
+  defp nothing?(input), do: String.trim(input) == ""
+  defp yelling?(input) do
+    # String is all upper-case and has at least one letter.
+    String.upcase(input) == input && String.match?(input, ~r/\p{L}/u)
   end
 end
