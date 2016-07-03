2a98506e927e5111f11707371608dd68e6b0e442
Eric Biven <eric@biven.us>
2016-07-03 17:00:16 -0500

    Updated grains
    Took out my janky `pow` function.

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -5,7 +5,7 @@ defmodule Grains do
   """
   @spec square(pos_integer) :: pos_integer
   def square(number) do
-    pow(2, number - 1)
+    round(:math.pow(2, number - 1))
   end
 
   @doc """
@@ -14,13 +14,5 @@ defmodule Grains do
   @spec total :: pos_integer
   def total do
     Enum.reduce(1..64, 0, &(square(&1) + &2))
-  end
-
-  # Implement pow instead of using the Erlang function because I can.
-  def pow(_, 0), do: 1
-  def pow(n, p) when Integer.is_odd(p), do: n * pow(n, p - 1)
-  def pow(n, p) do
-    result = pow(n, div(p, 2))
-    result * result
   end
 end
