f81a681efccf85da0c475dbd460d481bccb0ff76
Eric Biven <eric@biven.us>
2016-07-07 17:53:27 -0500

    JS prime-factors

---
+++ javascript/prime-factors/prime-factors.js
@@ -0,0 +1,18 @@
+module.exports = {
+  for (n, result=[]) {
+    if (n < 2) { return []; }
+
+    let limit = Math.sqrt(n);
+    let x = 2;
+
+    if (n % x) {
+     x = 3;
+     while ((n % x != 0) && ((x = x + 2) < limit)) {}
+    }
+
+    x = (x <= limit) ? x : n;
+    result.push(x);
+
+    return (x == n) ? result : this.for(n / x, result);
+  }
+};
