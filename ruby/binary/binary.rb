e88b1c7a03cbbbeef1c1b2fe63402c97a88a07e0
Eric Biven <eric@biven.us>
2016-07-17 17:53:19 -0500

    Ruby binary

---
+++ ruby/binary/binary.rb
@@ -0,0 +1,19 @@
+class Binary
+  def initialize(bin)
+    raise ArgumentError if bin =~ /[^01]/
+    @value = bin
+  end
+
+  def to_decimal
+    @value
+      .chars
+      .reverse
+      .collect(&:to_i)
+      .each_with_index
+      .inject(0) { |acc, (i,j)| acc += i * 2**j }
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
