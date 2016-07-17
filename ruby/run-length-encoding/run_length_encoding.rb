89ac00d22a0294dc0ebf6c7a1f9493387f2ac6f0
Eric Biven <eric@biven.us>
2016-07-17 17:11:04 -0500

    Ruby run-length-encoding

---
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -0,0 +1,25 @@
+module RunLengthEncoding
+  def self.encode(input)
+    input.chars.chunk { |i| i }.each_with_object('') do |item, acc|
+      if item[1].length == 1
+        acc << item[0]
+      else
+        acc << "#{item[1].length}#{item[0]}"
+      end
+    end
+  end
+
+  def self.decode(input)
+    input.scan(/(\d+)?(\D)/).each_with_object('') do |item, acc|
+      if item[0].to_i > 1
+        acc << item[1]*item[0].to_i
+      else
+        acc << item[1]
+      end
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
