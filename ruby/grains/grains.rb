e0cdfd45d9f5d9c4b9820853028fe884e1c08915
Eric Biven <eric@biven.us>
2016-07-17 12:11:07 -0500

    Ruby updated grains

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -3,7 +3,7 @@ module Grains
     2**(num - 1)
   end
 
-  def self.total
-    (1..64).inject(0) { |sum, i| sum += square(i) }
+  def self.total(to=64)
+    (1..to).inject(0) { |sum, i| sum += square(i) }
   end
 end
