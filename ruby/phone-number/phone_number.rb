9cb1f1f24f6ea08362ebb92975eefc929374e538
Eric Biven <eric@biven.us>
2016-07-17 21:34:13 -0500

    Ruby updated phone-number

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -3,15 +3,9 @@ class PhoneNumber
 
   def initialize(raw)
     r = raw.gsub(/[ ()\.-]/, '').match(/^1?(\d{3})(\d{3})(\d{4})$/)
-    if r
-      @area_code   = r.captures[0]
-      @office_code = r.captures[1]
-      @extension   = r.captures[2]
-    else
-      @area_code   = '000'
-      @office_code = '000'
-      @extension   = '0000'
-    end
+    @area_code   = r ? r.captures[0] : '000'
+    @office_code = r ? r.captures[1] : '000'
+    @extension   = r ? r.captures[2] : '0000'
     @number = @area_code + @office_code + @extension
   end
 
