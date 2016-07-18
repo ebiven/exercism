e1b99b927740d1c3517838aca8a64d405fcf224f
Eric Biven <eric@biven.us>
2016-07-17 19:40:10 -0500

    Ruby updated accumulate

--- ruby/accumulate/accumulate.rb
+++ ruby/accumulate/accumulate.rb
@@ -1,8 +1,7 @@
 class Array
   def accumulate
-    # This could be an accumulator array having a value pushed on
-    # in an `each`, but everything is a pretty thin wrapper around
-    # map.
-    map { |i| yield(i) }
+    out = []
+    each { |i| out.push(yield(i)) }
+    out
   end
 end
