76f07620e17c8e2dec48ffaee9f9ae32a766b376
Eric Biven <eric@biven.us>
2016-07-19 23:54:40 -0500

    Ruby updated proverb

--- ruby/proverb/proverb.rb
+++ ruby/proverb/proverb.rb
@@ -2,10 +2,7 @@ class Proverb
   @phrase = ""
 
   def initialize(*items, qualifier: false)
-    pairs = (0..items.length-2).each_with_object([]) do |i, acc|
-      acc.push(items.slice(i, 2))
-    end
-    @phrase = pairs
+    @phrase = items.each_cons(2)
       .map { |(i,j)| "For want of a #{i} the #{j} was lost." }
       .join("\n")
     want_of = (qualifier) ? "#{qualifier} #{items.first}" : "#{items.first}"
