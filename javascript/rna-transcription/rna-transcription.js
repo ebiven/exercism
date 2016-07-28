ae4f00a1d25aa963d6b9e7d01f9393dfdf6b3771
Eric Biven <eric@biven.us>
2016-07-28 11:33:06 -0500

    JS updated rna-transcription

--- javascript/rna-transcription/rna-transcription.js
+++ javascript/rna-transcription/rna-transcription.js
@@ -1,15 +1,17 @@
 var DnaTranscriber = function() {};
 
-DnaTranscriber.prototype.translations = {
-  "A": "U",
-  "C": "G",
-  "T": "A",
-  "G": "C",
+DnaTranscriber.translate = function(i) {
+  return {
+    "A": "U",
+    "C": "G",
+    "T": "A",
+    "G": "C",
+  }[i];
 };
 
 DnaTranscriber.prototype.toRna = function(strand) {
   return [...strand]
-    .map((i) => this.translations[i])
+    .map(DnaTranscriber.translate)
     .join("");
 };
 
