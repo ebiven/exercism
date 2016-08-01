449d498e32a4ad265aa4ad0289951e3965bfa41a
Eric Biven <eric@biven.us>
2016-08-01 18:33:44 -0500

    JS updated trinary

--- javascript/trinary/trinary.js
+++ javascript/trinary/trinary.js
@@ -4,13 +4,13 @@ const Trinary = function(value) {
 };
 
 Trinary.prototype.toDecimal = function() {
-  if (!this.value.match(/^[012]+$/)) { return 0; }
-  let out = 0;
-  let remaining = this.value.length;
-  [...this.value].forEach((i) => {
-    out += i * Math.pow(3, --remaining);
-  });
-  return out;
+  return DecimalConverter(this.value, /^[0-2]+$/, 3);
 };
 
 module.exports = Trinary;
+
+const DecimalConverter = function(value, matchRegex, base) {
+  if (!value.match(matchRegex)) { return 0; }
+  return [...value].reverse().reduce((pV, cV, index) =>
+    pV + cV * Math.pow(base, index), 0);
+};
