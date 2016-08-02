daf629ebd4cad09d10226b4f9b3f473c15290533
Eric Biven <eric@biven.us>
2016-08-02 15:41:13 -0500

    JS updated hamming

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -4,11 +4,7 @@ Hamming.prototype.compute = function(a, b) {
   // Short circuit an exact match.
   if (a === b) { return 0; }
   if (a.length !== b.length) { throw "DNA strands must be of equal length."; }
-  let diff = 0;
-  for (let i = 0; i <= a.length; i++) {
-    if (a[i] !== b[i]) { diff++; }
-  }
-  return diff;
+  return [...a].reduce((pV, cV, i) => (cV == b[i]) ? pV : ++pV, 0);
 };
 
 module.exports = Hamming;
