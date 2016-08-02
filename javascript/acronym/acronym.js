a73a7236a7066b9058d38bfe63292fa991596e03
Eric Biven <eric@biven.us>
2016-08-02 09:08:14 -0500

    JS updated acronym

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -1,13 +1,8 @@
-var Acronym = function() {};
+const Acronym = function() {};
 
 Acronym.parse = function(input) {
-  let out = "";
   let parts = input.match(/[A-Z]+[a-z]*|[a-z]+/g);
-  parts.forEach((part) => {
-    out += part.charAt(0).toUpperCase();
-  });
-
-  return out;
+  return parts.reduce((acc, c) => acc += c[0].toUpperCase(), '');
 };
 
 module.exports = Acronym;
