9faa189b3bd6e8f2ca409bde640d5631a1a810fe
Eric Biven <eric@biven.us>
2016-08-02 09:03:08 -0500

    JS updated pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,19 +1,11 @@
 var Pangram = function(sentence) {
-  this.sentence = sentence.toLowerCase().replace(/[^a-z]/g, '').split('');
+  this.sentence = sentence.toLowerCase().replace(/[^a-z]/g, '')
 };
 
 Pangram.prototype.isPangram = function() {
   return "abcdefghijklmnopqrstuvwxyz".split('').every((l) => {
-    return this.sentence.some((m) => m == l)
+    return this.sentence.includes(l);
   });
-  //let letters = [];
-  //[...this.sentence]
-    //.forEach((l) => {
-      //if (!letters.includes(l)) {
-        //letters.push(l);
-      //}
-    //});
-  //return letters.length == 26;
 };
 
 module.exports = Pangram;
