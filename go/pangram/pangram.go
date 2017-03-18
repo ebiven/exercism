a6ecb767d0736e053bf7715659fe4ceb4673b34e
Eric Biven <eric@biven.us>
2017-03-18 13:29:28 -0500

    Go pangram

---
+++ go/pangram/pangram.go
@@ -0,0 +1,20 @@
+package pangram
+
+import (
+	"strings"
+)
+
+const testVersion = 1
+
+var letters = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+
+func IsPangram(input string) bool {
+	input = strings.ToLower(input)
+	for _, l := range letters {
+		if !strings.Contains(input, l) {
+			return false
+		}
+	}
+
+	return true
+}
