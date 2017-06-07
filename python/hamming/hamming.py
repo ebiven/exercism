7ad52e81928adcf622d11a252e74f7586d935b65
Eric Biven <eric@biven.us>
2017-06-07 18:53:15 -0500

    Python hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,6 @@
-def distance():
-    pass
+def distance(strand1, strand2):
+    if len(strand1) != len(strand2):
+        raise ValueError
+
+    zipped = zip(strand1, strand2)
+    return sum(1 for [a, b] in zipped if a != b)
