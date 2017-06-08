dc19cfeb2afd35f82ac5221a21641b83dc1871f3
Eric Biven <eric@biven.us>
2017-06-07 20:13:53 -0500

    Python bob update.

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -2,17 +2,7 @@ import re
 
 def hey(phrase):
     phrase = phrase.strip()
-
-    if is_yelling(phrase):  return 'Whoa, chill out!'
-    if is_question(phrase): return 'Sure.'
-    if is_nothing(phrase):  return 'Fine. Be that way!'
+    if phrase.isupper():     return 'Whoa, chill out!'
+    if phrase.endswith('?'): return 'Sure.'
+    if len(phrase) == 0:     return 'Fine. Be that way!'
     return 'Whatever.'
-
-def is_yelling(phrase):
-    return phrase.upper() == phrase and re.search('[A-Z]', phrase)
-
-def is_question(phrase):
-    return phrase.endswith('?')
-
-def is_nothing(phrase):
-    return len(phrase) == 0
