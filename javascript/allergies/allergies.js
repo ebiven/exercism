ee343f395abcdbaa1f60886653577a3c0e64f83a
Eric Biven <eric@biven.us>
2017-06-11 18:50:38 -0500

    Javascript updated allergies

--- javascript/allergies/allergies.js
+++ javascript/allergies/allergies.js
@@ -2,23 +2,23 @@ const Allergies = function(flags) {
   this.flags = flags;
 };
 
-Allergies.allergens = {
-  "eggs"         : 1,
-  "peanuts"      : 2,
-  "shellfish"    : 4,
-  "strawberries" : 8,
-  "tomatoes"     : 16,
-  "chocolate"    : 32,
-  "pollen"       : 64,
-  "cats"         : 128
-}
+Allergies.allergens = [
+  "eggs",
+  "peanuts",
+  "shellfish",
+  "strawberries",
+  "tomatoes",
+  "chocolate",
+  "pollen",
+  "cats"
+];
 
 Allergies.prototype.list = function() {
-  return Object.keys(Allergies.allergens).filter((k) => this.allergicTo(k));
+  return Allergies.allergens.filter((k) => this.allergicTo(k));
 };
 
 Allergies.prototype.allergicTo = function(item) {
-  return (Allergies.allergens[item] & this.flags) != 0;
+  return (this.flags & 1 << Allergies.allergens.indexOf(item)) != 0;
 };
 
 module.exports = Allergies;
