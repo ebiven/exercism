89662a2da6ac3398ce707a6433abeccb80ba36bc
Eric Biven <eric@biven.us>
2017-06-12 19:10:03 -0500

    Python updated series

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,6 @@
-def slices():
-    pass
+def slices(source, size):
+    if len(source) < size or size < 1:
+        raise ValueError
+
+    source = list(map(int, source))
+    return [source[i:i+size] for i in range(len(source)-size+1)]
