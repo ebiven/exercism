ca35001408f0683b2911de4bb6fe03e827aafd3d
Eric Biven <eric@biven.us>
2017-06-23 17:45:58 -0500

    Python scrabble-score

---
+++ python/scrabble-score/scrabble_score.py
@@ -0,0 +1,15 @@
+# Human-readable score map.
+SCORE_MAP = {
+    1  : 'aeioulnrst',
+    2  : 'dg',
+    3  : 'bcmp',
+    4  : 'fhvwy',
+    5  : 'k',
+    8  : 'jx',
+    10 : 'qz'
+}
+# Transposed for easier scoring.
+POINTS = { v: k for k, vs in SCORE_MAP.items() for v in vs }
+
+def score(word):
+    return sum([ POINTS[l] for l in word.lower() ])
